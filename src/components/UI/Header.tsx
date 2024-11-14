7474146f21b7baf0feb452855b5bd3b82f87d487
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-14 19:54:43 +0200

    tweaked the design and fixed it

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,10 +1,57 @@
 import "./Header.css";
+import { useState } from "react";
+import { useUser } from "../../providers/UserProvider";
 
-export function Header() {
+type HeaderProps = {
+  setPage: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export function Header({ setPage }: HeaderProps) {
+  const { currentUser, isAdmin, logout } = useUser();
+  const [isMenuShown, setIsMenuShown] = useState(false);
+
+  function handleClick() {
+    setIsMenuShown(!isMenuShown);
+  }
+
   return (
     <header className="header">
       <div className="logo">Logo</div>
-      <button className="logout-button">Logout</button>
+      <button onClick={handleClick} className="logout-button">
+        {currentUser?.name}
+      </button>
+
+      {isMenuShown && (
+        <ul className="user-menu">
+          {isAdmin && (
+            <li key="vote">
+              <button
+                onClick={() => {
+                  setPage("vote");
+                  setIsMenuShown(false);
+                }}
+              >
+                Vote
+              </button>
+            </li>
+          )}
+          {isAdmin && (
+            <li key="admin">
+              <button
+                onClick={() => {
+                  setPage("admin");
+                  setIsMenuShown(false);
+                }}
+              >
+                Admin
+              </button>
+            </li>
+          )}
+          <li>
+            <button onClick={logout}>Logout</button>
+          </li>
+        </ul>
+      )}
     </header>
   );
 }
