d81b0c992e2f7b933a8eb97204cc0712d61ce620
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-14 19:54:05 +0200

    created the admin page and its design

--- src/components/AdminPage/AdminPage.tsx
+++ src/components/AdminPage/AdminPage.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import "./AdminPage.css";
+import { useUser } from "../../providers/UserProvider";
 
 export default function AdminPage() {
-  return <div>AdminPage</div>;
+  const { getUsers } = useUser();
+
+  const users = getUsers();
+
+  if (!users) return;
+
+  return (
+    <div className="table-container">
+      <div className="table-header">
+        <h4>Name</h4>
+        <h4>Status</h4>
+      </div>
+      <table className="admin-table">
+        <tbody>
+          {users.map((u) => (
+            <tr key={u.name} className={`${u.isVoted ? "voted" : ""}`}>
+              <td>{u.name}</td>
+              <td>{u.isVoted ? "Voted!" : "Did Not Vote"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
