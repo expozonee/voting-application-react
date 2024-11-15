4ce37adbd4b30ba964324aa531a90acdf69f9291
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-15 17:07:21 +0200

    removed the placeholder data with actual data for the candidates

--- src/components/VoteContainer/VoteContainer.tsx
+++ src/components/VoteContainer/VoteContainer.tsx
@@ -1,19 +1,12 @@
 import "./VoteContainer.css";
-
 import { VoteCard } from "../VoteCard/VoteCard";
-
-const candidates = [
-  { id: 1, name: "Candidate 1" },
-  { id: 2, name: "Candidate 2" },
-  { id: 3, name: "Candidate 3" },
-  { id: 4, name: "Candidate 4" },
-];
+import candidates from "../../data/candidates.json";
 
 export function VoteContainer() {
   return (
     <div className="vote-container">
       {candidates.map((c) => {
-        return <VoteCard key={c.id} name={c.name} />;
+        return <VoteCard key={c.id} name={c.name} imageUrl={c.imageUrl} />;
       })}
     </div>
   );
